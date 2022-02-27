6aea2f3258f87d81bee281fc2ebf027f619d5835
Rasha <rasha.abdessalam@gmail.com>
2022-02-27 23:33:03 +0200

    27th

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,13 +5,13 @@
 * License: https://bootstrapmade.com/license/
 */
 
-fetch('https://julianyasin.herokuapp.com/')
+fetch('https://julianyasin.herokuapp.com/jj')
   .then(response => response.json())
   .then(json => {
       var mainul = document.getElementById('mainul')
       for (let i = 0; i < json.length; i++) {
           var li = document.createElement('li')
-          li.innerHTML = json [i];
+          li.innerHTML = json [i].no;
           li.class="list-group-item"
           mainul.appendChild(li)
       }
