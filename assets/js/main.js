75e671ab6157c74fd114639757500d90cb445aad
Rasha <rasha.abdessalam@gmail.com>
2022-03-01 19:50:49 +0200

    33th1st

--- assets/js/main.js
+++ assets/js/main.js
@@ -193,30 +193,30 @@
     items: 1
   });
 
-  var xValues = [100,200,300,400,500,600,700,800,900];
+  // var xValues = [100,200,300,400,500,600,700,800,900];
 
-              new Chart("myChart", {
-                  type: "line",
-                  data: {
-                labels: xValues,
-                datasets: [{ 
-                  data: [860,1140,1060,1060,1070,1110,1330,2210,7830],
-                  borderColor: "black",
-                  fill: false
-                }, { 
-                  data: [1600,1700,1700,1900,2000,2700,4000,5000,6000],
-                  borderColor: "green",
-                  fill: false
-                }, { 
-                  data: [300,700,2000,5000,6000,4000,2000,1000,200],
-                  borderColor: "blue",
-                  fill: false
-                }]
-              },
-                options: {
-                legend: {display: false}
-                }
-              });
+  //             new Chart("myChart", {
+  //                 type: "line",
+  //                 data: {
+  //               labels: xValues,
+  //               datasets: [{ 
+  //                 data: [860,1140,1060,1060,1070,1110,1330,2210,7830],
+  //                 borderColor: "black",
+  //                 fill: false
+  //               }, { 
+  //                 data: [1600,1700,1700,1900,2000,2700,4000,5000,6000],
+  //                 borderColor: "green",
+  //                 fill: false
+  //               }, { 
+  //                 data: [300,700,2000,5000,6000,4000,2000,1000,200],
+  //                 borderColor: "blue",
+  //                 fill: false
+  //               }]
+  //             },
+  //               options: {
+  //               legend: {display: false}
+  //               }
+  //             });
 
 })(jQuery);
 
