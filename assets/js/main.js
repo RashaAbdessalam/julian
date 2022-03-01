9b25c7ef4987f9f683de531a3aa3e71e0237c7b3
Rasha <rasha.abdessalam@gmail.com>
2022-03-01 20:59:25 +0200

    jj

--- assets/js/main.js
+++ assets/js/main.js
@@ -193,30 +193,28 @@
     items: 1
   });
 
-  var xValues = [100,200,300,400,500,600,700,800,900];
+  var xValues = [50,60,70,80,90,100,110,120,130,140,150];
+var yValues = [7,8,8,9,9,9,10,11,14,14,15];
 
-              new Chart("myChart", {
-                  type: "line",
-                  data: {
-                labels: xValues,
-                datasets: [{ 
-                  data: [860,1140,1060,1060,1070,1110,1330,2210,7830],
-                  borderColor: "red",
-                  fill: false
-                }, { 
-                  data: [1600,1700,1700,1900,2000,2700,4000,5000,6000],
-                  borderColor: "green",
-                  fill: false
-                }, { 
-                  data: [300,700,2000,5000,6000,4000,2000,1000,200],
-                  borderColor: "blue",
-                  fill: false
-                }]
-              },
-                options: {
-                legend: {display: false}
-                }
-              });
+new Chart("myChart", {
+  type: "line",
+  data: {
+    labels: xValues,
+    datasets: [{
+      fill: false,
+      lineTension: 0,
+      backgroundColor: "rgba(0,0,255,1.0)",
+      borderColor: "rgba(0,0,255,0.1)",
+      data: yValues
+    }]
+  },
+  options: {
+    legend: {display: false},
+    scales: {
+      yAxes: [{ticks: {min: 6, max:16}}],
+    }
+  }
+});
 
 })(jQuery);
 
