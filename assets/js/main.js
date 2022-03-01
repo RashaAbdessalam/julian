af299f88d3a2af5993be0c6abd4991abf40d2c30
Rasha <rasha.abdessalam@gmail.com>
2022-03-01 18:45:32 +0200

    32nd1st

--- assets/js/main.js
+++ assets/js/main.js
@@ -201,7 +201,7 @@
                 labels: xValues,
                 datasets: [{ 
                   data: [860,1140,1060,1060,1070,1110,1330,2210,7830],
-                  borderColor: "red",
+                  borderColor: "black",
                   fill: false
                 }, { 
                   data: [1600,1700,1700,1900,2000,2700,4000,5000,6000],
